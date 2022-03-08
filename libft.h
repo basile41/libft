744ad25595b0c7329f48e2e5548b5388e77d7ca0
Basile REGNEAU <bregneau@student.42.fr>
2022-03-08 18:38:20 +0100

    add ft_split_f.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 18:27:08 by bregneau          #+#    #+#             */
-/*   Updated: 2022/03/08 18:04:09 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/03/08 18:31:04 by bregneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	**ft_split_f(char const *s, int (*f)(int));
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
